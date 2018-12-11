89a72cb1d5a80353af878457171b97ddc9fd1ed3
jomondo <joanmodaw@gmail.com>
2018-12-11 10:59:54 +0100

    some changes

--- src/components/catalog/catalog.js
+++ src/components/catalog/catalog.js
@@ -1,23 +1,17 @@
 /** @module ComponentsApp */
 import React from 'react';
 import Families from "../catalog/families";
-import {Utils} from "../../utils";
 
 /**
  * @class
  * Draw article catalog that could be filtered by article family
  */
 class Catalog extends React.Component {
-    /**
-     * @constructor
-     * @param {json} datosEmpresaJSON 
-     * @param {string} selectRule 
-     */
     constructor(props) {   
         super(props);
-        this.state={
+        /*this.state={
             families:[]
-        };
+        };*/
     }
 
     componentDidMount(){
@@ -25,7 +19,6 @@ class Catalog extends React.Component {
     }
     /** render  */
     render() {
-
         return (
             <Families/>
         );
--- src/components/catalog/families.js
+++ src/components/catalog/families.js
@@ -5,7 +5,7 @@ import { Utils } from "../../utils";
 
 /**
  * @class
- * Draw cookies text information
+ * Draw families on catalog
  */
 class Families extends React.Component {
     constructor(props) {
@@ -13,18 +13,32 @@ class Families extends React.Component {
         this.state = {
             families: [],
             currentFamily: [],
+            text: [],
+            color: [],
+            subtexto: [],
             isLoading: true
         }
     }
     componentDidMount() {
         let that = this;
+        let text = [], subtexto = [];
         Utils.get('/familia').then(function (response) {
-            let currentFamily = response.results[0];
-            let familia = response.results;
+            var currentFamily = response.results[0];
+            var currentFamilyFilter = [response.results[0]];
+            let family = response.results;
+            currentFamilyFilter.filter((itemText) => {
+                let cabecera = itemText.texto_cabecera.replace(/^\<p>+|\<\/p>+$/g, '');
+                text.push(cabecera);
+                let subCabecera = itemText.subtexto_cabecera.replace(/^\<p>+|\<\/p>+$/g, '');
+                subtexto.push(subCabecera);
+            });
 
             that.setState({
-                families: familia,
+                families: family,
                 currentFamily: currentFamily,
+                text: text.join(""),
+                subtexto: subtexto.join(""),
+                color: currentFamily.color.hexadecimal,
                 isLoading: false
             });
         }).catch(function (error) {
@@ -32,18 +46,18 @@ class Families extends React.Component {
         });
 
     }
-    /** render  */
+    /** render */
     render() {
         const isLoading = this.state.isLoading;
         return (
             <div>
-                <div className="pt-4 pb-4 pr-0 pl-0 m-0 text-white" style={{ background: "#C7BB15" }}>
+                <div className="pt-4 pb-4 pr-0 pl-0 m-0 text-white" style={{ background: this.state.color }}>
                     <img className="float-left" src={this.state.currentFamily.imagen_cabecera} />
                     <h3>{this.state.currentFamily.pretitulo}</h3>
                     <h1 className="display-1">{this.state.currentFamily.titulo}</h1><br /><br />
-                    <h3>{this.state.currentFamily.texto_cabecera}</h3>
+                    <h3>{this.state.text}</h3>
                     <h1 className="display-3">{this.state.currentFamily.precio_cabecera} € IVA Inc.</h1>
-                    <h3>{this.state.currentFamily.subtexto_cabecera}</h3>
+                    <h3>{this.state.subtexto}</h3>
                 </div>
                 <div className="grid row">
                     <div className="col">
@@ -57,16 +71,14 @@ class Families extends React.Component {
                             isLoading ? (
                                 <h1>Loading families...</h1>
                             ) : (
-                                    <li className="nav-item">
+                                    <li className="nav-item" key={itemFamily.codfamilia}>
                                         <a id="change-family" className='nav-link {index==0?"active":""}' data-toggle="pill" href={'#catalog/' + itemFamily.codfamilia}><img width="32px" height="32px" src={itemFamily.icono} />{itemFamily.nombre}</a>
                                     </li>
                                 ));
                     })}
 
                 </ul>
-
             </div>
-
         );
     }
 }
