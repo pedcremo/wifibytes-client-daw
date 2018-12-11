4e4100ac3e03a3d0ea9d867860a46c13839e3f94
jomondo <joanmodaw@gmail.com>
2018-12-11 12:01:09 +0100

    join components articles and filters

--- src/components/catalog/catalog.js
+++ src/components/catalog/catalog.js
@@ -1,28 +1,49 @@
 /** @module ComponentsApp */
 import React from 'react';
+import Articles from "./articles";
 import Families from "../catalog/families";
+import Filters from "./filters";
+import {Utils} from "../../utils";
 
 /**
  * @class
  * Draw article catalog that could be filtered by article family
  */
 class Catalog extends React.Component {
-    constructor(props) {   
+    constructor(props) {
         super(props);
         /*this.state={
             families:[]
         };*/
+        this.state = {
+            filters: [],
+            articles: [],
+            isLoading: true
+        };
     }
 
-    componentDidMount(){
-        
+    componentDidMount() {
+        let that = this;
+        Promise.all([Utils.get("/filtros"), Utils.get("/articulo")]).then(function (results) {
+            that.setState({
+                filters: results[0],
+                articles: results[1].results,
+                isLoading: false
+            });
+        }).catch(function (error) {
+            console.log("Failed!", error);
+        });
     }
     /** render  */
     render() {
         return (
-            <Families/>
+            <span>
+            <Families />
+            <Filters filters={this.state.filters} />
+            <Articles articles={this.state.articles} />
+            </span>
         );
-                  
+
     }
 }
 
