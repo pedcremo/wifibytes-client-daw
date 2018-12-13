62c6f7a0da6d4cc1b45aaa0085e26ddde52422a2
Moises Muriana Goya <murianamoises@gmail.com>
2018-12-13 19:43:20 +0100

    Created new login components

---
+++ src/components/login/forgotPasword.js
@@ -0,0 +1,30 @@
+/** @module ComponentsApp */
+import React from 'react';
+import {Utils} from "../utils";
+
+/**
+ * @class
+ * Draw Login. A form to login
+ */
+class forgotPassword extends React.Component  {
+    /**
+     * @constructor
+     */
+    constructor() { 
+        super();
+	}
+	
+	componentDidMount(){
+
+    }
+ 
+    /** render  */
+    render() {  
+		return (
+			<div>
+
+			</div>
+		);
+    }
+}
+export default forgotPassword;
