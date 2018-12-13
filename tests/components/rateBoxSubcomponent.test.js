14f7e177b02e83b4aacfd604c223f995a1f68461
gionaico <gmc.yanez@gmail.com>
2018-12-13 17:15:17 +0100

    Changing RateRateSubcomponent test

--- tests/components/rateBoxSubcomponent.test.js
+++ tests/components/rateBoxSubcomponent.test.js
@@ -1,30 +1,34 @@
-import tarifaJSON from '../json_endpoints/tarifa.json';
 import React from 'react';
-import renderer from 'react-test-renderer';
 import RateBoxSubComponent from '../../src/components/rateBoxSubcomponent';
-import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-configure({ adapter: new Adapter() });
-import { shallow, mount, render } from "enzyme";
+import tarifaJSON from '../json_endpoints/tarifa.json';
+import { shallow, mount, configure } from "enzyme";
 
-const $ = require('jquery');
+configure({ adapter: new Adapter() });
+
 
 describe('<RateBoxSubComponent/>', () => {
 
     it('RateBoxSubComponent component renders the RateBoxSubComponent correctly', () => {
-        
-        const rendered = renderer.create(<RateBoxSubComponent rates={tarifaJSON} />);
-        expect(rendered.toJSON()).toMatchSnapshot();
+        const rendered = mount(<RateBoxSubComponent rates={tarifaJSON} />)
+        expect(rendered).toMatchSnapshot()
     });
 
-    it('Check if there is just one component when rendering is done', () => {
+    it('Check if there is just one component (RateBoxSubComponent) when rendering is done', () => {
         const component = shallow(<RateBoxSubComponent rates={tarifaJSON} />);
         expect(component).toHaveLength(1);
     });
 
-    it("Check if there are 1 or more divs with a class border-dark ", () => {
-      let rateBox = shallow(<RateBoxSubComponent rates={tarifaJSON} />);
-      expect(rateBox.find("div.border-dark").length).toBeGreaterThan(0);
+    it("Check if there are 1 or more divs with a class border-dark (rates boxes)", () => {
+        const rateBox = shallow(<RateBoxSubComponent rates={tarifaJSON} />);
+        expect(rateBox.find("div.border-dark").length).toBeGreaterThan(0);
+    });
+
+    it("Component must fail due to JSON input doesnt contains expected information", () => {
+        const rateBox = shallow(<RateBoxSubComponent rates={[]} />);
+        expect(function () {
+            rateBox
+        }).toThrowError(undefined);
     });
 });
 
