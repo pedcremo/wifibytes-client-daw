796e30f6e1cf46b6c68e0e1cc156d88690285aa1
cesar muriana goya <cesarmuriana@gmail.com>
2018-12-14 16:17:06 +0100

    Making login, and solve any bugs on css

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -19,9 +19,12 @@ class Login extends React.Component  {
     }
     recoverPass(){
         let recover = document.getElementById("login-recover");
-        
-        
-
+        /**
+         * 
+         * It check if hidden class is in recovery div and if it is in it, it will remove it 
+         * and add class with animation and see visible it and vice versa
+         * 
+         * */
             if (recover.classList.contains("login-recuperar-input")){
                 recover.classList.toggle("login-recuperar-input")
                 recover.classList.add("login-recov-pass");
@@ -44,8 +47,12 @@ class Login extends React.Component  {
                         <h4>{Utils.translate("login-acces-password")}</h4>
                         <input className="loginInput" placeholder={Utils.translate("login-acces-password-form")}></input>
                         <button className="login-button btn">{Utils.translate("login-button-acces")}</button>
-                        <p className="login-recuperar" onClick={this.recoverPass}>He olvidado mi contraseña</p>
-                        <input id="login-recover" className="login-recov-pass" placeholder={Utils.translate("login-recover-password")}></input>
+                        <a className="login-button btn left" href={"#/register"}>{Utils.translate("login-button-register")}</a>
+                        <p className="login-recuperar" onClick={this.recoverPass}>{Utils.translate("login-text-recover")}</p>
+                        <div id="login-recover" className="login-recov-pass">
+                            <input className="loginInput" placeholder={Utils.translate("login-recover-password")}></input>
+                            <button className="login-button btn">{Utils.translate("login-button-recover")}</button>
+                        </div>
                     </div>
                 </div>
             </div>
