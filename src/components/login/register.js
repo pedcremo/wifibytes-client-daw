0c959ed8d102bf18195b89b3cb4847b1ed7cde71
Moises Muriana Goya <murianamoises@gmail.com>
2018-12-14 20:10:24 +0100

    Working in Register

--- src/components/login/register.js
+++ src/components/login/register.js
@@ -23,32 +23,46 @@ class Register extends React.Component  {
 		return (
 			<section>
                 <form>
-                    {/* NOM */}
-                    <input placeholder=""></input>
-
-                    {/* COGNOMS */}
-                    <input placeholder=""></input>
-
-
-                    {/* CORREU */}
-                    <input placeholder=""></input>
-
-
-                    {/* PASSW */}
-                    <input placeholder=""></input>
-
-
-                    {/* PASSW */}
-                    <input placeholder=""></input>
-
-                    
-                    {/* CHECK POLITICA PRIV */}
-                    <input></input>
-
-
-                    {/* CHECK NO REBRE OFERTES */}
-                    <input></input>
-
+                    <div>
+                        <td>
+                            {/* NOM */}
+                            <h4>{Utils.translate("register-name")}</h4>
+                            <input placeholder={Utils.translate("register-name")}></input>
+                        </td>
+                        <td>
+                            {/* COGNOMS */}
+                            <h4>{Utils.translate("register-surname")}</h4>
+                            <input placeholder={Utils.translate("register-surname")}></input>
+                        </td>
+                        <td>
+                            {/* CORREU */}
+                            <h4>{Utils.translate("register-email")}</h4>
+                            <input placeholder={Utils.translate("register-email")}></input>
+                        </td>
+                    </div>
+                    <div>
+                        <td>
+                            {/* PASSW */}
+                            <h4>{Utils.translate("register-passs")}</h4>
+                            <input placeholder={Utils.translate("register-passs")}></input>
+                        </td>
+                        <td>
+                            {/* PASSW */}
+                            <h4>{Utils.translate("register-repeatPassw")}</h4>
+                            <input placeholder={Utils.translate("register-repeatPassw")}></input>
+                        </td>
+                    </div>
+                    <div>
+                        <td>
+                            {/* CHECK POLITICA PRIV */}
+                            <input type="checkbox"></input>
+                            <a href="/#legal"> {Utils.translate("register-legal")} </a>
+                        
+                            {/* CHECK NO REBRE OFERTES */}
+                            <input type="checkbox"></input>
+                            <a> {Utils.translate("register-newslater")}</a>
+                        </td>
+                    </div>
                 </form>
 			</section>
 		);
