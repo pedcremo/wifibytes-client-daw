94e38c55fd1c5f965d7d196b658e5804907af54f
cesar muriana goya <cesarmuriana@gmail.com>
2018-12-14 01:17:32 +0100

    Making login and is hided horizontal navigation bar

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -15,7 +15,21 @@ class Login extends React.Component  {
 	}
 	
 	componentDidMount(){
+        
+    }
+    recoverPass(){
+        let recover = document.getElementById("login-recover");
+        
+        
 
+            if (recover.classList.contains("login-recuperar-input")){
+                recover.classList.toggle("login-recuperar-input")
+                recover.classList.add("login-recov-pass");
+            }else if (!recover.classList.contains("login-recuperar-input")){
+                recover.classList.toggle("login-recov-pass")
+                recover.classList.add("login-recuperar-input");
+            }
+          
     }
  
     /** render  */
@@ -29,6 +43,9 @@ class Login extends React.Component  {
                         <input className="loginInput" placeholder={Utils.translate("login-acces-dni-form")}></input>
                         <h4>{Utils.translate("login-acces-password")}</h4>
                         <input className="loginInput" placeholder={Utils.translate("login-acces-password-form")}></input>
+                        <button className="login-button btn">{Utils.translate("login-button-acces")}</button>
+                        <p className="login-recuperar" onClick={this.recoverPass}>He olvidado mi contraseña</p>
+                        <input id="login-recover" className="login-recov-pass" placeholder={Utils.translate("login-recover-password")}></input>
                     </div>
                 </div>
             </div>
