8f323349394472a9b599c4c7f9b99585e18c93af
Moises Muriana Goya <murianamoises@gmail.com>
2018-12-16 20:13:40 +0100

    Register work in progress and add google captcha

--- src/components/login/register.js
+++ src/components/login/register.js
@@ -1,6 +1,7 @@
 /** @module ComponentsApp */
 import React from 'react';
 import {Utils} from "../../utils";
+import ReCAPTCHA from "react-google-recaptcha";
 
 /**
  * @class
@@ -12,60 +13,78 @@ class Register extends React.Component  {
      */
     constructor() { 
         super();
+        this.recaptchaRef = React.createRef();
 	}
 	
 	componentDidMount(){
 
     }
- 
+
     /** render  */
-    render() {  
+    render() {          
 		return (
-			<section>
+			<section id="register">
+                <h1>{Utils.translate("register-title")}</h1>
                 <form>
-                    <div>
-                        <td>
+                    <div className="register--containers">
+                        <div className="register--content">
                             {/* NOM */}
                             <h4>{Utils.translate("register-name")}</h4>
                             <input placeholder={Utils.translate("register-name")}></input>
-                        </td>
-                        <td>
+                        </div>
+                        <div className="register--content">
                             {/* COGNOMS */}
                             <h4>{Utils.translate("register-surname")}</h4>
                             <input placeholder={Utils.translate("register-surname")}></input>
-                        </td>
-                        <td>
+                        </div>
+                        <div className="register--content">
                             {/* CORREU */}
                             <h4>{Utils.translate("register-email")}</h4>
                             <input placeholder={Utils.translate("register-email")}></input>
-                        </td>
+                        </div>
                     </div>
-                    <div>
-                        <td>
+                    <div className="register--containers">
+                        <div className="register--content">
                             {/* PASSW */}
                             <h4>{Utils.translate("register-passs")}</h4>
                             <input placeholder={Utils.translate("register-passs")}></input>
-                        </td>
-                        <td>
+                        </div>
+                        <div className="register--content">
                             {/* PASSW */}
                             <h4>{Utils.translate("register-repeatPassw")}</h4>
                             <input placeholder={Utils.translate("register-repeatPassw")}></input>
-                        </td>
+                        </div>
                     </div>
-                    <div>
-                        <td>
+                    <div className="register--checkbox">
+                        <div>
                             {/* CHECK POLITICA PRIV */}
                             <input type="checkbox"></input>
-                            <a href="/#legal"> {Utils.translate("register-legal")} </a>
-                        
+                            <a href="/#legal"> {Utils.translate("register-legal")} </a>    
+                        </div>
+                        <div>
                             {/* CHECK NO REBRE OFERTES */}
                             <input type="checkbox"></input>
                             <a> {Utils.translate("register-newslater")}</a>
-                        </td>
+                        </div>
                     </div>
+                    <div id="captcha">
+                        <ReCAPTCHA
+                        ref={this.recaptchaRef}
+                        sitekey="6LesI4IUAAAAAHHx9B6OuMjqpVl9bIyLOT3n4y3C"
+                        />
+                    </div>
+
+                    <button class="login-button btn" onClick={console.log('button')}>{Utils.translate("register-button")}</button>
+
                 </form>
 			</section>
 		);
+    }
+
+    submit (text) {
+        console.log(text);
+        console.log('aqui');
+        alert(text);
     }
 }
 export default Register;
