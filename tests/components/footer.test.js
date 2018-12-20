89033f3a191de8a6f869d261e83f1e6abc0e5a5f
Rallkus <serhuegi@gmail.com>
2018-12-20 17:14:12 +0100

    Footer test working with redux

--- tests/components/footer.test.js
+++ tests/components/footer.test.js
@@ -1,23 +1,40 @@
 import Footer from "../../src/components/footer";
+import datosEmpresaJSON from '../json_endpoints/datos_empresa.json';
+import homeJSON from "../json_endpoints/home.json";
 import React from 'react';
 import {Utils} from '../../src/utils';
-import datosEmpresaJSON from "../json_endpoints/datos_empresa.json";
-import homeJSON from "../json_endpoints/home.json";
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import configureStore from 'redux-mock-store';
+import { Provider } from "react-redux";
+
 Enzyme.configure({ adapter: new Adapter() });
 
 jest.mock('../../src/utils');
+const mockStore = configureStore();
+const initialState = {datosEmpresaJSON, homeJSON};
+Utils.getUserLang.mockReturnValue("va");
 
-const resp1 = datosEmpresaJSON;
-const resp2 = homeJSON;
-Utils.get.mockResolvedValueOnce(resp1);
-Utils.get.mockResolvedValueOnce(resp2);            
-Utils.get.mockResolvedValue(resp1);
-const footer = Enzyme.shallow(<Footer />);
+const store = mockStore(initialState);
+
+const footer = Enzyme.shallow(<Provider store={store}><Footer /></Provider>);
+
+describe('<Footer />', () => {
+  test('dispatches event to show footer data', () => {
+    expect(footer).toMatchSnapshot(); 
+    console.log(footer.props().value.storeState.homeJSON);
+    expect(footer.props().value.storeState.datosEmpresaJSON.name).toMatch(/Wifibytes S.L/);
+    expect(footer.props().value.storeState.homeJSON[0].titulo).toMatch(/Wifibytes/);
+    expect(footer.props().value.storeState.homeJSON[1].caja_izquierda_texto).toMatch(/Do you want to cancel subscription?/);
+    expect(footer.props().value.storeState.datosEmpresaJSON.textos).toHaveLength(13);
+    expect(store.getActions()).toMatchSnapshot();
+    
+  });
+});
 
 
-describe('<footer />', () => {
+
+/*describe('<footer />', () => {
 
   it("We can check if Footer component called the class constructor", () => {
     expect(footer).toMatchSnapshot();
@@ -28,4 +45,4 @@ describe('<footer />', () => {
       expect(footer.find('#companyName').getElement().props.children).toBe("Wifibytes S.L");
       expect(footer.find('.left_box').getElement().props.dangerouslySetInnerHTML.__html).toMatch(/<p>Vols donar-te de baixa/);
   });
-});
+});*/
