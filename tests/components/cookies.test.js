4ced3d0bb814606efa5bc38e895126295b2ec0a8
Rallkus <serhuegi@gmail.com>
2018-12-20 16:56:50 +0100

    removed utils from cookies and legal tests

--- tests/components/cookies.test.js
+++ tests/components/cookies.test.js
@@ -1,7 +1,6 @@
 import Cookies from '../../src/components/cookies';
 import datosEmpresaJSON from '../json_endpoints/datos_empresa.json';
 import React from 'react';
-import {Utils} from '../../src/utils';
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import configureStore from 'redux-mock-store';
@@ -9,10 +8,8 @@ import { Provider } from "react-redux";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-jest.mock('../../src/utils');
 const mockStore = configureStore();
 const initialState = datosEmpresaJSON;
-Utils.getUserLang.mockReturnValue("va");
 
 const store = mockStore(initialState);
 
