c37566c1f2b388a4c6a519c5c6b7c650bb53f34c
Alejandro Vañó Tomás <avato92@gmail.com>
2018-12-20 17:56:06 +0100

    Fixed some errors

--- tests/components/login.test.js
+++ tests/components/login.test.js
@@ -1,4 +1,4 @@
-import Login from '../../src/components/login/login';
+import Login from '../../src/components/login/loginComponent';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Enzyme from 'enzyme';
