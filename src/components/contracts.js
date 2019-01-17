8fff204dca4644525d289f54a51a2f3aff661088
Dani Ortiz <daniortizgar@gmail.com>
2019-01-17 19:31:03 +0100

    Injectar variables en el HTML, Modal simulando PDF

--- src/components/contracts.js
+++ src/components/contracts.js
@@ -1,9 +1,7 @@
 
 /** @module ComponentsApp */
 import React from 'react';
-import {Utils} from "../utils";
 import { connect } from "react-redux";
-import { getDatosEmpresa } from "../actions/datosEmpresaActions";
 
 /**
  * @class
@@ -11,23 +9,54 @@ import { getDatosEmpresa } from "../actions/datosEmpresaActions";
  */
 class Contracts extends React.Component {
 
-    componentDidMount(){
-        this.props.dispatch(getDatosEmpresa());
-    }
-
     /** render  */
     render() {
-        return(
+
+        let pdf = '<p><strong>AUTORIZACION CUENTA BANCARIA PARA ADEUDOS</strong></p><p>D. ${person.name} con nº NIF ${person.NIF} y domicilio en C/ ${person.direccion}, de ${person.ciudad}. provincia de ${person.provincia}. declara subsistentes las facultades con las que interviene que en modo alguno le han sido revocadas, modificadas ni suspendidas y , en la calidad que actúa,</p> <p><strong>AUTORIZA</strong></p><p> a la empresa WIFIBYTES, S.L , provista de CIF B98137078 a que desde la fecha de la presente y con carácter indefinido en tanto continúen las relaciones comerciales entre la empresa y el cliente, a que gire en el número de cuenta bancaria especificada en la presente autorización, todos los recibos correspondientes a las facturas que se originen como consecuencia de conexión de internet establecida, según lo exigido por la Ley de Servicios de Pago 16/2009.</p><p><strong>DATOS DE CONFIRMACION DE LA ENTIDAD BANCARIA</strong></p><p>Nombre de la Entidad Bancaria:</p><p>Domicilio de la Entidad Bancaria:</p><p>Nº de cuenta:</p><p>Confirma mediante firma:</p><p>Fecha: El ${person.day} de ${person.month} de ${person.year} </p>';
+        let person = {
+            name: "Daniel Ortiz Garcia",
+            NIF: "49264590Q",
+            direccion: "Avenida Almaig",
+            ciudad: "Ontinyent",
+            provincia: "Valencia",
+            day: new Date().getDay(),
+            month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][new Date().getMonth()],
+            year: new Date().getFullYear()
+        }
+
+        return (
             <div>
                 <h1>Contracts</h1>
-            </div>);
+                <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+                    Launch demo modal
+                </button>
+                <div className="modal fade" id="exampleModalLong" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLong" aria-hidden="true">
+                    <div className="modal-dialog modal-lg" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLong">Firmar Contratos</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <p dangerouslySetInnerHTML={{ __html: eval('`' + pdf + '`') }}></p>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" className="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        );
+
     }
 }
 
 const mapStateToProps = state => ({
-    datosEmpresa: state.datosEmpresa.items,
-    loading: state.datosEmpresa.loading,
-    error: state.datosEmpresa.error
 });
 
 export default connect(mapStateToProps)(Contracts);
