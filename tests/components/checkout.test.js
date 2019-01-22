18dc77fedceb68e1b263131f906b06766a8694f4
Hibernon Puig Soler <hiber98@gmail.com>
2019-01-22 21:58:26 +0100

    accepting changes

--- tests/components/checkout.test.js
+++ tests/components/checkout.test.js
@@ -5,6 +5,7 @@ import Adapter from 'enzyme-adapter-react-16'
 import configureStore from 'redux-mock-store';
 import { Provider } from "react-redux";
 import * as checkoutActions from '../../src/actions/checkoutActions';
+import currentCheckout from '../../src/reducers/checkoutReducer';
 
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -71,7 +72,80 @@ describe('<Checkout />', () => {
     
         store.dispatch(checkoutActions.updateStep(1));
         expect(store.getActions()).toEqual(selectedActions);
+    });  
+
+    it('Dispatches the correct action and payload', () => {
+        const store = mockStore(initialState);
+        const add = [
+        {
+            'type': 'ADD_STEPS',
+            'payload': {
+                step: 1,
+                steps: ['step1','step2']
+            },
+        },
+        ];
+
+        store.dispatch(checkoutActions.addSteps(1, ['step1','step2']));
+        expect(store.getActions()).toEqual(add);
     });
 
+});
 
+describe('Reducer', () => {
+    it('should return the initial state', () => {
+        expect(currentCheckout(undefined, {})).toEqual(
+            {
+                currentStep: 0,
+                steps: [],
+                loading: false,
+            }
+        )
+    })
+
+    it('should handle ADD_STEPS', () => {
+        expect(
+            currentCheckout([], {
+                type: checkoutActions.ADD_STEPS,
+                payload: {
+                    currentStep: 1,
+                    steps: ['pas1', 'pas2']
+                }
+            })
+        ).toEqual(
+            {
+                currentStep: 1,
+                steps: ['pas1', 'pas2'],
+                loading: false
+            }
+        )
+
+    })
+
+
+    it('should handle UPDATE_STEP', () => {
+        expect(
+            currentCheckout([
+                    {
+                        currentStep: 0,
+                        steps: [],
+                        loading: false
+                    }
+                ], {
+                type: checkoutActions.UPDATE_STEP,
+                payload: {
+                    currentStep: 1,
+                    steps: ['pas1', 'pas2'],
+                }
+                
+            })
+        ).toEqual([
+            {
+                currentStep: 1,
+                steps: ['pas1', 'pas2'],
+                loading: false
+            }
+        ])
+
+    })
 });
