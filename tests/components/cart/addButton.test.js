b51be65618adc644d28f889f9bda8a66fb83306d
Josep Soler Pla <josepsolerpla@gmail.com>
2019-01-31 19:23:52 +0100

    AddButton test

--- tests/components/cart/addButton.test.js
+++ tests/components/cart/addButton.test.js
@@ -2,16 +2,14 @@ import AddButton from "../../../src/components/cart/AddButton";
 import React from 'react';
 import Enzyme from 'enzyme'
 import { mount , shallow} from 'enzyme'
-<<<<<<< HEAD
 import configureStore from 'redux-mock-store';
-=======
->>>>>>> e60a24eb75425e799ce2c151a94534a750fcbb2d
 import Adapter from 'enzyme-adapter-react-16'
+import { setItem } from '../../../src/components/cart/cartActions'
+
 window.$ = require('jquery');
 
 Enzyme.configure({ adapter: new Adapter() });
 
-<<<<<<< HEAD
 const mockStore = configureStore();
 const initialState = {
   items: [],
@@ -68,36 +66,3 @@ describe('<AddButton />', () => {
     expect(store.getActions()).toEqual(selectedActions);
   })
 })
-=======
-it("We can check if AddButton component called the class constructor", () => {
-  const rendered = Enzyme.shallow(<AddButton id={15} price={15} description={"Hola"} text={"buy"}/>);
-  expect(rendered).toMatchSnapshot();
-});
-
-it("AddButton render must be called and it works properly", () => {
-  const wrapper = Enzyme.shallow(<AddButton id={15} price={15} description={"Hola"} text={"buy"}/>);
-  const instance = wrapper.instance();
-  wrapper.update();
-  instance.render();
-  console.log(instance.render())
-  expect(wrapper.find('button').length).toBe(1);
-});
-
-it(' _renderToastrs invoke  _renderToastrs right amount of times', () => {
-  const wrapper = Enzyme.shallow(<AddButton id={15} price={15} description={"Hola"} text={"buy"}/>);
-  const instance = wrapper.instance();
-  instance.addItem = jest.fn();
-  wrapper.update();
-  instance.render();
-
-  expect(instance.addItem).toHaveBeenCalledTimes(
-    instance.addItem.length
-  );
-})
-// it("When click Change Set Render", () => {
-//   const rendered = Enzyme.shallow(<AddButton id={15} price={15} description={"Hola"} text={"buy"}/>);
-//   rendered.instance()
-//   rendered.find('button').simulate('click',{ preventDefault() {} })
-//   expect(rendered.state('items').length).toBe(1);
-// });
->>>>>>> e60a24eb75425e799ce2c151a94534a750fcbb2d
