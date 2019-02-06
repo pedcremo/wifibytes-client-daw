3a36a12649c9925c7bef052ac976338e28a56fe9
Hibernon Puig Soler <hiber98@gmail.com>
2019-02-06 08:51:46 +0100

    solved error

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -10,4 +10,3 @@ export const SET_ITEM = 'SET_ITEM';
 export const GET_ITEMS = 'GET_ITEMS'
 export const DEL_ITEM = 'DEL_ITEM';
 export const SET_QUANTITY = 'SET_QUANTITY';
-export const UPDATE_DATA = 'UPDATE_DATA';
