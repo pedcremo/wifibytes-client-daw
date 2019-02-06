11e1a898ee99c57b7f173359cc3fc5564b22c155
nicklaus_ <pedcremo@gmail.com>
2019-02-06 19:41:27 +0100

    comment initial  store state

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,10 @@ import rootReducer from "../reducers/rootReducer";
 import { localStorage , saveJWT, promiseMiddleware , isAuth} from '../middleware'
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+/** We initialize store state with localStorage redux-store entry.
+ * It is a persistence backaup of our state if we close browser
+ * 
+*/
 export const store = createStore(
     rootReducer,
     (typeof(Storage) && window.localStorage.getItem('redux-store')) ?
@@ -16,8 +20,7 @@ export const store = createStore(
 store.subscribe(() => {
     
     // Check browser support
-    if (typeof(Storage) !== "undefined") {
-        console.log("LOCALSTORAGE CHANGE");
+    if (typeof(Storage) !== "undefined") {        
         window.localStorage.setItem('redux-store',JSON.stringify(store.getState()));
     }
 })
