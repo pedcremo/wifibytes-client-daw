2185a52d86b6cec5ef8e52eb2c4e8db83a1da858
nicklaus_ <pedcremo@gmail.com>
2019-02-06 20:11:09 +0100

    rates restored vegas caroussel

--- src/components/rates.js
+++ src/components/rates.js
@@ -103,6 +103,7 @@ class Rates extends React.Component {
                     </nav>
 
                     <RateBoxSubComponent rates={this.state.rates}></RateBoxSubComponent>
+                    <VegasCarousel vegas={datosEmpresa} />
 
                     <div className="card-deck mt-2 mb-5">
                         {boxTextsArray}
