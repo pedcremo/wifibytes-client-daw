a4cdd2a0accbdde34adde5db99900d62bd219900
jomondo <joanmodaw@gmail.com>
2019-02-07 17:58:08 +0100

    add css button next step

--- src/components/checkout/checkout.js
+++ src/components/checkout/checkout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from "react-redux"
 import { Route } from 'react-router-dom'
 import { Step } from 'semantic-ui-react'
-import {Agent} from './agent';
+import { Agent } from './agent';
 import PropTypes from 'prop-types'
 
 import steps from "./libraries/steps";
@@ -15,27 +15,27 @@ import {
     NEXT_STEP,
     PREVIOUS_STEP,
     UPDATE_STEP
-  } from '../../constants/actionTypes';
+} from '../../constants/actionTypes';
 
 const mapDispatchToProps = dispatch => ({
     addSteps: (step, steps) =>
-      dispatch({ type: ADD_STEPS, payload:{ step, steps } }),
+        dispatch({ type: ADD_STEPS, payload: { step, steps } }),
     /**
      * Go to the next step
      */
     nextStep: () =>
-      dispatch({ type: NEXT_STEP }),
+        dispatch({ type: NEXT_STEP }),
     /**
      * Go to the previous step
      */
     previousStep: () =>
-      dispatch({ type: PREVIOUS_STEP }),
+        dispatch({ type: PREVIOUS_STEP }),
     /**
      * Sets a specific step
      */
     setStep: (step) =>
-      dispatch({ type: UPDATE_STEP, payload: { step } })
-  });
+        dispatch({ type: UPDATE_STEP, payload: { step } })
+});
 
 /**
  * Component Checkout validate the steps you have to follow
@@ -50,56 +50,57 @@ class Checkout extends React.Component {
     /**
      * Agent filters cart items and returns an array used to filter the steps to achieve the needed ones
      */
-    componentDidMount(){
-        if(this.props.cartItems.items != null && JSON.parse(localStorage.getItem('cartReducer')) != null) {
+    componentDidMount() {
+        if (this.props.cartItems.items != null && JSON.parse(localStorage.getItem('cartReducer')) != null) {
             let stepsRates;
-            this.props.cartItems.items.length !== 0?
-            stepsRates = Agent.objectsToArray(this.props.cartItems.items, library):
-            stepsRates = Agent.objectsToArray(JSON.parse(localStorage.getItem('cartReducer')).items, library);
+            this.props.cartItems.items.length !== 0 ?
+                stepsRates = Agent.objectsToArray(this.props.cartItems.items, library) :
+                stepsRates = Agent.objectsToArray(JSON.parse(localStorage.getItem('cartReducer')).items, library);
             let filteredSteps = Agent.filterArray(steps, stepsRates);
             this.addSteps(1, filteredSteps);
         } else {
             this.context.router.history.push('/');
-        }  
+        }
     }
 
     componentDidUpdate() {
-        const self=this;
+        const self = this;
         const setID = (item, index) => {
-            item.id = index+1;
+            item.id = index + 1;
         };
-        const addClickEvent = (item, index) => item.addEventListener("click", function(){
-            self.setStep(index+1);
+        const addClickEvent = (item, index) => item.addEventListener("click", function () {
+            self.setStep(index + 1);
         });
-        
+
         document.querySelectorAll("div.step").forEach((item, index) => {
             setID(item, index);
             addClickEvent(item, index);
         });
     }
-    
-    sendOrder(){
-        let data = {"personal_data":{"name":"pepito","surname":"caball"},"contract":{"sd":"sdsd"},"confirm":{"asd":"sdsd"}};
-        Agent.ObjectSendToOrder(data,steps);
+
+    sendOrder() {
+        let data = { "personal_data": { "name": "pepito", "surname": "caball" }, "contract": { "sd": "sdsd" }, "confirm": { "asd": "sdsd" } };
+        Agent.ObjectSendToOrder(data, steps);
     }
     /**
      * Render prints the steps to follow and calls the function show step
      */
     render() {
         const { loading, steps, currentStep, nextStep } = this.props;
-        if (loading) 
+        if (loading)
             return (<div>Loading...</div>);
-        if (steps.length>0 && currentStep){
+        if (steps.length > 0 && currentStep) {
             return (
                 <div>
                     <Step.Group items={steps} attached='top' ordered />
-                    
-                    {steps[currentStep-1].component}
 
-                    {steps.length > currentStep?
-                        (<button onClick={nextStep}>
+                    {steps[currentStep - 1].component}
+
+                    {steps.length > currentStep ?
+                        (<button onClick={nextStep} class="ui right labeled icon button">
+                            <i class="right arrow icon"></i>
                             Next
-                        </button>):(
+                        </button>) : (
                         <button onClick={() => this.sendOrder()}>
                             Submit
                         </button>)
@@ -107,7 +108,7 @@ class Checkout extends React.Component {
                 </div>
             )
         } else {
-            return(
+            return (
                 <span>LOADING!</span>
             );
         }
