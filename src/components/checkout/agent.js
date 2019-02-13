6fa5b27f7d0eccf4e29645be86ae88ad276ba0f7
jomondo <joanmodaw@gmail.com>
2019-02-13 21:22:47 +0100

    fix method arrayToQuantityObject

--- src/components/checkout/agent.js
+++ src/components/checkout/agent.js
@@ -45,7 +45,7 @@ export const Agent = {
                     element[fieldRequire].forEach(item => {
                         library.fieldsToValidate.filter(el => {
                             let regexp = new RegExp(el.regexp);
-                            regexp.test(item.id) ? quantities[el.field] = quantities[el.field] + 1 * element.quantity : false;
+                            regexp.test(item.id) ? quantities[el.field]++ : false;
                         });
                     });
                 }
