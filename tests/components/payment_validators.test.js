28ed0b415bcd3593ce989f94b80fed0cd856697a
Moises Muriana Goya <murianamoises@gmail.com>
2019-02-13 10:24:36 +0100

    Update test

--- tests/components/payment_validators.test.js
+++ tests/components/payment_validators.test.js
@@ -1,33 +1,33 @@
-import {validations} from '../../src/validators/paymentFormValidators';
+import {Validations} from '../../src/validators/paymentFormValidators';
 
 it('Validate if expiration date is valid', () => {
-    expect(validations.expirationDateIsValid('02', '2022')).toBe(true);
+    expect(Validations.expirationDateIsValid('02', '2022')).toBe(true);
 });
 
 it('Validate if expiration date is false', () => {
-    expect(validations.expirationDateIsValid('02', '2018')).toBe(false);
+    expect(Validations.expirationDateIsValid('02', '2018')).toBe(false);
 }); 
     
 it('Validate if cvv is valid', () => {
-    expect(validations.cvvIsValid('123')).toBe(true);
+    expect(Validations.cvvIsValid('123')).toBe(true);
 });
 
 it('Validate if cvv is false', () => {
-    expect(validations.cvvIsValid('121113')).toBe(false);
+    expect(Validations.cvvIsValid('121113')).toBe(false);
 });
 
 it('Validate if Card Owner is valid', () => {
-    expect(validations.cardOwnerIsValid('nombre prueba')).toBe(true);
+    expect(Validations.cardOwnerIsValid('nombre prueba')).toBe(true);
 });
 
 it('Validate if Card Owner is false', () => {
-    expect(validations.cardOwnerIsValid('sa16sa1551')).toBe(false);
+    expect(Validations.cardOwnerIsValid('sa16sa1551')).toBe(false);
 });
 
 it('Validate if Card Number is false', () => {
-    expect(validations.cardNumberIsValid('4360846875643654')).toBe(true);
+    expect(Validations.cardNumberIsValid('4360846875643654')).toBe(true);
 });
 
 it('Validate if Card Number is false', () => {
-    expect(validations.cardNumberIsValid('sa16sa1551')).toBe(false);
+    expect(Validations.cardNumberIsValid('sa16sa1551')).toBe(false);
 });
