dc7c549fbc56eaac71410e01dd562b3d623aa1ff
Hibernon Puig Soler <hiber98@gmail.com>
2019-02-14 11:53:34 +0100

    tests

--- tests/components/checkout.test.js
+++ tests/components/checkout.test.js
@@ -6,13 +6,6 @@ import configureStore from 'redux-mock-store';
 import { Provider } from "react-redux";
 import * as checkoutActions from '../../src/constants/actionTypes';
 import currentCheckout from '../../src/reducers/checkoutReducer';
-// import {
-//     ADD_STEPS,
-//     NEXT_STEP,
-//     PREVIOUS_STEP,
-//     UPDATE_STEP
-// } from '../../src/constants/actionTypes';
-
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -40,6 +33,24 @@ const initialState = {
         }
     ]
 };
+
+const initialStateData = {
+    data: {
+        "cardOwner": "dfgdf df",
+        "cardNumber": "2020202020",
+        "expirationMonth": 2,
+        "expirationYear": 2019,
+        "cvv": 414
+    }
+}
+
+const buttonState = {
+    "currentStep": 1, 
+    "data": {}, 
+    "disabled": true,
+    "loading": false, 
+    "steps": []
+}
 
 const store = mockStore(initialState);
 
@@ -95,6 +106,7 @@ describe('<Checkout />', () => {
 
 });
 
+
 describe('Reducer', () => {
     it('should return the initial state', () => {
         expect(currentCheckout(undefined, {})).toEqual(
@@ -109,18 +121,47 @@ describe('Reducer', () => {
     })
 
     it('should handle ADD_STEPS', () => {
+        const store = mockStore(initialState);
         expect(
-            currentCheckout([], {
+            currentCheckout(store.getState(), {
                 type: checkoutActions.ADD_STEPS,
                 payload: {
                     currentStep: undefined,
-                    steps: ['pas1', 'pas2']
+                    steps: [
+                        {
+                            active: true, 
+                            className: "", 
+                            completed: true, 
+                            key: "personal_data", 
+                            title: "Dades Personals"
+                        },
+                        {
+                            active: false, 
+                            completed: false, 
+                            key: "contract", 
+                            title: "Contracte"
+                        }
+                    ]
                 }
             })
         ).toEqual(
             {
                 currentStep: undefined,
-                steps: ['pas1', 'pas2'],
+                steps: [
+                    {
+                        active: true, 
+                        className: "", 
+                        completed: true, 
+                        key: "personal_data", 
+                        title: "Dades Personals"
+                    },
+                    {
+                        active: false, 
+                        completed: false, 
+                        key: "contract", 
+                        title: "Contracte"
+                    }
+                ],
                 loading: false
             }
         )
@@ -301,4 +342,148 @@ describe('Reducer', () => {
         )
 
     })
+
+    it('should handle SET_COMPLETED', () => {
+        const store = mockStore(initialState);
+        expect(
+            currentCheckout(store.getState(), {
+                type: checkoutActions.SET_COMPLETED
+            })
+        ).toEqual(
+            {
+                currentStep: 1, 
+                steps: [
+                    {
+                        active: false, 
+                        className: "", 
+                        completed: true, 
+                        key: "personal_data", 
+                        title: "Dades Personals"
+                    }, 
+                    {
+                        active: true, 
+                        completed: false, 
+                        key: "contract", 
+                        title: "Contracte"
+                    }, 
+                    {
+                        active: false, 
+                        completed: false, 
+                        key: "confirm", 
+                        title: "Confirmar Pedido"
+                    }
+                ]
+            }
+            
+        )
+    })
+
+    it('should handle SET_UNCOMPLETED', () => {
+        const store = mockStore(initialState);
+        expect(
+            currentCheckout(store.getState(), {
+                type: checkoutActions.SET_UNCOMPLETED
+            })
+        ).toEqual(
+            {
+                currentStep: 1, 
+                steps: [
+                    {
+                        active: false, 
+                        className: "error", 
+                        completed: false, 
+                        key: "personal_data", 
+                        title: "Dades Personals"
+                    }, 
+                    {
+                        active: true, 
+                        completed: false, 
+                        key: "contract", 
+                        title: "Contracte"
+                    }, 
+                    {
+                        active: false, 
+                        completed: false, 
+                        key: "confirm", 
+                        title: "Confirmar Pedido"
+                    }
+                ]
+            }
+            
+        )
+    })
+
+    it('should handle UPDATE_DATA', () => {
+        const store = mockStore(initialStateData);
+        expect(
+            currentCheckout(undefined, {
+                type: checkoutActions.UPDATE_DATA,
+                payload: {
+                    key: "payment",
+                    data: {
+                        cardNumber: "7777777777",
+                        cardOwner: "paco",        
+                        expirationMonth: 2,
+                        expirationYear: 2019,
+                        cvv: 414
+                    }
+                },
+
+            })
+        ).toEqual(
+            {
+                currentStep: 0, 
+                data: 
+                {
+                    payment: 
+                    {
+                        cardNumber: "7777777777", 
+                        cardOwner: "paco",
+                        expirationMonth: 2,
+                        expirationYear: 2019,
+                        cvv: 414
+                    }
+                }, 
+                disabled: true, 
+                loading: false, 
+                steps: []
+            }
+            
+        )
+    })
+
+    it('should handle DISABLE_BUTTON', () => {
+        const store = mockStore(buttonState);
+        expect(
+            currentCheckout(store.getState(), {
+                type: checkoutActions.DISABLE_BUTTON
+            })
+        ).toEqual(
+            {
+                currentStep: 1, 
+                data: {}, 
+                disabled: true, 
+                loading: false, 
+                steps: []
+            }
+        )
+    })
+
+    it('should handle ACTIVATE_BUTTON', () => {
+        const store = mockStore(buttonState);
+        expect(
+            currentCheckout(store.getState(), {
+                type: checkoutActions.ACTIVATE_BUTTON
+            })
+        ).toEqual(
+            {
+                currentStep: 1, 
+                data: {}, 
+                disabled: false, 
+                loading: false, 
+                steps: []
+            }
+        )
+    })
+
 });
