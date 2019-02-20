6ad8f8d25bcb9b467dc5eeb3423707a5c64745c6
Rallkus <serhuegi@gmail.com>
2019-02-20 21:49:18 +0100

    Now datosHomeActions uses again cross-fetch

--- src/actions/datosHomeActions.js
+++ src/actions/datosHomeActions.js
@@ -5,15 +5,13 @@ import fetch from 'cross-fetch';
 
 export function getDatosHome() {
   //console.warn("getDatosHome")
-    return dispatch => {
-      dispatch(getDatosHomeBegin());
-      return Utils.get("/home",[ Utils.filterPruneArrayByLang,"lang"])
-        .then(function(response) {
-          dispatch(getDatosHomeSuccess(response));
-          return response;
-        })
-        .catch(error => dispatch(getDatosHomeFailure(error)));
-    };
+  return dispatch => {
+    dispatch(getDatosHomeBegin());
+    return fetch(`${Settings.baseURL}/home`)
+      .then(response => response.json())
+      .then(data => dispatch(getDatosHomeSuccess(Utils.filterPruneArrayByLang(data, "lang"))))
+      .catch(error => dispatch(getDatosHomeFailure(error)));
+  };
 }
 
 
